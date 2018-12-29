4c80d6c9a94f865b0af4412c3fbe646ef3e2a4d8
Rahul Jain <rahuljain@Rahuls-MacBook-Air.local>
2018-12-29 20:00:25 +0530

    Updated bubblesort

--- Lists/bubblesort.py
+++ Lists/bubblesort.py
@@ -1,8 +1,11 @@
 def bubblesort(lst):
     for i in range(0, len(lst) -1):
+        count = 0
         for j in range(len(lst)-1,i,-1):
             if lst[j] < lst[j-1]:
                 lst[j],lst[j-1] = lst[j-1],lst[j]
+        if count == 0 :
+            break
 
 
 def main():
