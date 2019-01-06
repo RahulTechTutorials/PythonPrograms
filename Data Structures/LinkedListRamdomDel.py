25251ce8b7839d30cca1fd513cffbc1c84b0c803
Rahul Jain <rahuljain@Rahuls-MacBook-Air.local>
2019-01-06 12:00:22 +0530

    This program is to illustrate how to delete the node randomly

---
+++ Data Structures/LinkedListRamdomDel.py
@@ -0,0 +1,93 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    def insertBegin(self,value):
+        if self.head == None:
+            self.head = Node(value)
+        else:
+            temp = Node(value)
+            temp.next = self.head
+            self.head = temp
+        print('New Linkedlist/Node created ')
+    def delBegin(self):
+        if self.head == None:
+            print('Empty Linked list')
+        else:
+            temp = self.head
+            value = self.head.data
+            self.head = self.head.next
+            del temp
+            print(value,' deleted')
+    def delVal(self,value):
+        
+        if self.head == None:
+            print('Linked list is empty')
+        else:
+            deleteflag = 'No'
+            current = self.head
+            prev = None
+            while current != None:
+                if current.data == value:
+                    if prev == None:
+                        temp = self.head
+                        self.head = self.head.next
+                        del temp
+                        print('Value Deleted: ',value)
+                        deleteflag = 'Yes'
+                        break
+                    else:
+                        temp = current
+                        prev.next = current.next
+                        del current
+                        print('Value Deleted: ',value)
+                        deleteflag = 'Yes'
+                        break
+                else:
+                    prev = current
+                    current = current.next
+            if deleteflag == 'No':
+                print(value,' Not present in the linkedlist')
+    def llprint(self):
+        current = self.head
+        print('Current Address  data   Next Address   Next')
+        while current != None:
+            print(id(current), '\t', current.data, '\t', id(current.next),'\t',current.next )
+            current = current.next
+        return None
+            
+        
+        
+def main():
+    print('Enter a choice to perform the desired operation:')
+    print('1: Insert in the begining')
+    print('2: Delete from begining')
+    print('3: Delete a specific value')
+    print('4: Print the Linkedlist')
+    print('5: quit')
+    
+    lst = Linkedlist()
+    while 1:
+        choice = int(input('Enter Choice: '))
+        if choice == 1 :
+            val = eval(input('Enter the Value for the node'))
+            lst.insertBegin(val)
+        elif choice ==2:
+            lst.delBegin()
+        elif choice ==3:
+            val = eval(input('Enter the Value to be deleted'))
+            lst.delVal(val)
+        elif choice == 4:
+            lst.llprint()
+        elif choice == 5:
+            break
+
+            
+     
+if __name__ == '__main__':
+    main()
+            
